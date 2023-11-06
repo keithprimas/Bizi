ff48b3fb7abddb1b2151197a862bc44cdd14894e
AleSolis24 <aletitan24@icloud.com>
2023-11-05 21:02:55 -0500

    we getting somewhere

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api/'); // prob need to check this
-const hompageroutes = require('./homepage-routes.js'); // this too 
-const userRoutes = require('./api/user-router')
+const apiRoutes = require('./api'); // prob need to check this
+const hompageroutes = require('./homepage-routes'); // this too 
+//const userRoutes = require('./api/user-router')
 
 // need to check if these work properly 
+router.use('/', hompageroutes);
 router.use('/api', apiRoutes);
-router.use('/home', hompageroutes);
-router.use('/login', userRoutes);
+//router.use('/login', userRoutes);
 
 module.exports = router;
 
