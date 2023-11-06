a6bb4e090486436b8feeb01bffd42b0e21f81f7f
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-11-06 18:55:00 -0500

    fixed login and signup routes

--- DB/login.sql
+++ DB/login.sql
@@ -4,8 +4,8 @@ USE login_db;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
+  firstname VARCHAR(255),
+  lastname VARCHAR(255),
   email VARCHAR(255),
   password VARCHAR(255)
 );
