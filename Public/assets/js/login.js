f6d0e2339d5ad5da0e6db78f2e91e2c6762a2d17
AleSolis24 <aletitan24@icloud.com>
2023-11-06 18:59:47 -0500

    updatE

--- Public/assets/js/login.js
+++ Public/assets/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async (event) => {
   if (email && password) {
     try {
       // need to find the right api - Alejandro  
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
