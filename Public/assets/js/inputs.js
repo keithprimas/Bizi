3012a439d298a13d05593110d59efe72245a2102
Benjamin Palacpac <ben.palacpac@gmail.com>
2023-11-06 20:05:37 -0500

    updated routes for inputsm page

---
+++ Public/assets/js/inputs.js
@@ -0,0 +1,37 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const user_name = document.querySelector('#user_name').value;
+    const email_address = document.querySelector('#email_address').value;
+    const phone_number = document.querySelector('#phone_number').value;
+    const location = document.querySelector('#location').value;
+    const personal = document.querySelector('#personal').value;
+    const title = document.querySelector('#title').value;
+    const company = document.querySelector('#company').value;
+
+    const response = await fetch(`/api/`, {
+        method: 'POST',
+        body: JSON.stringify({
+            user_name,
+            email_address,
+            phone_number,
+            location,
+            personal,
+            title,
+            company,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    });
+    
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to add info');
+      }
+}
+    
+document
+  .querySelector('#inputs-section')
+  .addEventListener('submit', newFormHandler);
---
+++ controllers/api/inputs-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const Input = require('../../Models'); 
+// need to set path for what Input needs to require. '../../Models'??
+
+// route to create a card
+router.post('/', async (req, res) => {
+  try {
+    const inputData = await Input.create({
+      user_name: req.body.user_name,
+      email_address: req.body.email_address,
+      phone_number: req.body.phone_number,
+      location: req.body.location,
+      personal: req.body.personal,
+      title: req.body.title,
+      company: req.body.company,
+    });
+    res.status(200).json(inputData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+    // sending the data to the Model so user info can be updated with new data in the database.
+    try {
+      const input = await Input.update(
+        {
+            user_name: req.body.user_name,
+            email_address: req.body.email_address,
+            phone_number: req.body.phone_number,
+            location: req.body.location,
+            personal: req.body.personal,
+            title: req.body.title,
+            company: req.body.company,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+     
+      // updated data is sent back to handler that dispatched the fetch request.
+      res.status(200).json(input);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+module.exports = router;
